88e85234d8592b2661f1426af90bce4b2d2381b7
ashish.mishra <ashish.mishra@gdn-commerce.com>
2018-06-24 20:41:57 +0530

    added squad_user feature

--- app/controllers/home/contestDetail.ctrl.js
+++ app/controllers/home/contestDetail.ctrl.js
@@ -8,29 +8,57 @@
 ;
 (function() {
  falcon
-    .controller('ContestDetailCtrl', ['$scope', '$state' ,'CommonService', 'UserService',
-    	function($scope, $state ,CommonService, UserService) {
+    .controller('ContestDetailCtrl', ['$scope', '$state' ,'CommonService', 'UserService', '$stateParams',
+    	function($scope, $state ,CommonService, UserService, $stateParams) {
         $scope.root.user.activeContestId = window.localStorage.getItem('contestId');
         $scope.contestDetail = {};
+        var isfalconUser = true;
 
     	function init(){
             $scope.root.userSelected = "Contest Details";
             $scope.contestDetail.title = "this is title";
             $scope.contestDetail.details = [];
+            isfalconUser = !getParameterByName('squad_user', window.location.href) || true;
             getContestDetail();
     	}
 
+        function getParameterByName(name, url) {
+            if (!url) url = window.location.href;
+            name = name.replace(/[\[\]]/g, "\\$&");
+            var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+                results = regex.exec(url);
+            if (!results) return null;
+            if (!results[2]) return '';
+            return decodeURIComponent(results[2].replace(/\+/g, " "));
+        }
+
         function getContestDetail(){
-            UserService.getContestDetail(localStorage.getItem('contestId')).then(
-                function(response){
-                    $scope.contestDetail.details = response.data.responseObject;
-                    $scope.contestDetail.details.startDate = CommonService.tsToDateString($scope.contestDetail.details.startDate);
-                    $scope.contestDetail.details.endDate = CommonService.tsToDateString($scope.contestDetail.details.endDate);
-                },
-                function(err){
-                    $scope.contestDetail.details = [];
-                }
-            );
+            if(isfalconUser){
+                UserService.getContestDetail(localStorage.getItem('contestId')).then(
+                    function(response){
+                        $scope.contestDetail.details = response.data.responseObject;
+                        $scope.contestDetail.details.startDate = CommonService.tsToDateString($scope.contestDetail.details.startDate);
+                        $scope.contestDetail.details.endDate = CommonService.tsToDateString($scope.contestDetail.details.endDate);
+                    },
+                    function(err){
+                        $scope.contestDetail.details = [];
+                    }
+                );
+            }
+            else {
+                UserService.takeTest(localStorage.getItem('contestId')).then(
+                    function(response){
+                        $scope.contestDetail.details = response.data.responseObject;
+                        $scope.contestDetail.details.startDate = CommonService.tsToDateString($scope.contestDetail.details.startDate);
+                        $scope.contestDetail.details.endDate = CommonService.tsToDateString($scope.contestDetail.details.endDate);
+                        window.localStorage.setItem('userId', $scope.contestDetail.details.inviteeDTOs[0].candidateId);
+                    },
+                    function(err){
+                        $scope.contestDetail.details = [];
+                    }
+                );
+            }
+
         }
 
         $scope.attemptContest = function(){
