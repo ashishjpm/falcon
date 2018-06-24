27c3b2cf0e65197201e50a0045e2ea64549bdad6
anand <anand.kumar@gdn-commerce.com>
2018-06-25 01:27:57 +0530

    question creation fixed

--- app/controllers/admin/adminQuestionCreate.ctrl.js
+++ app/controllers/admin/adminQuestionCreate.ctrl.js
@@ -21,7 +21,7 @@
       $scope.adminQuestionCreate.testCaseDtos = [];
       $scope.adminQuestionCreate.categories = [];
       $scope.adminQuestionCreate.optionsInput = [];
-      $scope.adminQuestionCreate.optionSelection = [];
+      $scope.adminQuestionCreate.optionSelection = [false,false,false,false];
     }
 
     $scope.adminQuestionCreate.backToList = function(){
@@ -40,10 +40,10 @@
       $scope.adminQuestionCreate.data.categoryDtos = [];
       $scope.adminQuestionCreate.data.categoryDtos = $scope.adminQuestionCreate.categories.map((item) => ({"name": item}));  
       if($scope.adminQuestionCreate.data.type != 'CODING') {
-        $scope.adminQuestionCreate.data.options = [];
-       for(var i=0; i<3; i++) {
+       $scope.adminQuestionCreate.data.options = [];
+       for(var i=0; i<4; i++) {
           $scope.adminQuestionCreate.data.options.push({
-            "isCorrect": $scope.adminQuestionCreate.optionSelection[i],
+            "isCorrect": $scope.adminQuestionCreate.optionSelection == i,
             "order": i,
             "value": $scope.adminQuestionCreate.optionsInput[i]
           });
@@ -51,10 +51,10 @@
       } else {
         $scope.adminQuestionCreate.data.testCaseDtos = $scope.adminQuestionCreate.testCaseDtos;
       }
-
-      AdminService.createQuestion($scope.adminQuestionCreate.data).then(function(response){
-        $state.go('admin.question');
-      }, function(err){})
+      console.log("question ", $scope.adminQuestionCreate.data);
+      // AdminService.createQuestion($scope.adminQuestionCreate.data).then(function(response){
+      //   $state.go('admin.question');
+      // }, function(err){})
     }
     $scope.adminQuestionCreate.categoryAdded = function() {
       $scope.adminQuestionCreate.categories.push($scope.adminQuestionCreate.currentCategory);
